86acbe2a672f8775ccff67a0304846ad9a49c9b6
bernicelamgit <bernicelam912@gmail.com>
2026-01-13 22:04:53 -0500

    //

--- src/project.v
+++ src/project.v
@@ -17,8 +17,8 @@ module tt_um_uwasic_onboarding_bernice_lam (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uio_out = 0;
   assign uio_oe  = 8'hFF;
 
   wire [7:0] en_reg_out_7_0;
@@ -39,5 +39,19 @@ module tt_um_uwasic_onboarding_bernice_lam (
   );
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
+  wire [7:0] spi_received_val;
+
+  spi_peripheral my_spi (
+    .clk(clk),
+    .rst_n(rst_n),
+    .sclk(ui_in[0]),
+    .ncs(ui_in[1]),
+    .copi(ui_in[2]),
+    .pwm_val(spi_received_val)
+  ); 
+
+
+
+
 
 endmodule
