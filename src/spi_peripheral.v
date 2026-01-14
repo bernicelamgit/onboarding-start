3c51105e43c986bd91da2e9e9aa56198244db1e1
bernicelamgit <bernicelam912@gmail.com>
2026-01-14 10:48:45 -0500

    fixed spi module

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -6,6 +6,11 @@ module spi_peripheral (
     input wire sclk, 
     input wire ncs,
     input wire copi, 
+
+    output reg [7:0] en_reg_out_7_0,
+    output reg  [7:0]  en_reg_out_15_8,
+    output reg  [7:0]  en_reg_pwm_7_0,
+    output reg  [7:0]  en_reg_pwm_15_8,
     output reg [7:0] pwm_duty_cycle
 );
     reg [2:0] sclk_sync;
@@ -25,7 +30,8 @@ module spi_peripheral (
     end
 
     wire sclk_rising_edge = sclk_sync[2]==0 && sclk_sync[1]==1;
-    
+    wire ncs_rising_edge = (ncs_sync[2]== 0 && ncs_sync[1]==1);
+    wire ncs_falling_edge = (ncs_sync[2]== 1 && ncs_sync[1]==0);
     reg [15:0] shift_reg;
     reg [4:0] bit_counter;
 
@@ -33,12 +39,12 @@ module spi_peripheral (
         if (!rst_n) begin
             bit_counter <=5'd0;
             shift_reg<=16'd0;
-        end else if (ncs_sync[1]) begin
+        end else if (ncs_falling_edge) begin
             bit_counter <= 5'd0;
             shift_reg <= 16'd0;
-        end else  if(sclk_rising_edge) begin
-                bit_counter <= bit_counter + 1'b1;
-                shift_reg <= {shift_reg[14:0], copi_sync[1]};
+        end else  if(!ncs_sync[1] && sclk_rising_edge) begin
+            bit_counter <= bit_counter + 1'b1;
+            shift_reg <= {shift_reg[14:0], copi_sync[2]};
             
 
             
@@ -46,13 +52,26 @@ module spi_peripheral (
     end
 
     localparam MAX_ADDRESS = 7'h04;
-    wire ncs_rising_edge = (ncs_sync[2]== 0 && ncs_sync[1]==1);
-    wire transaction_ready = ncs_rising_edge && bit_counter == 5'd16 && shift_reg[14:8]<=MAX_ADDRESS;
+    
+    wire transaction_ready = ncs_rising_edge && bit_counter == 5'd16 && shift_reg[15] == 1'b1;
+    
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
+            en_reg_out_7_0 <= 8'b0;
+            en_reg_out_15_8 <= 8'b0;
+            en_reg_pwm_7_0 <= 8'b0;
+            en_reg_pwm_15_8 <= 8'b0;
             pwm_duty_cycle <= 8'b0;
         end else if (transaction_ready) begin
-            pwm_duty_cycle <= shift_reg [7:0];
+            case(shift_reg[14:8])
+                7'h00: en_reg_out_7_0  <= shift_reg[7:0];
+                7'h01: en_reg_out_15_8 <= shift_reg[7:0];
+                7'h02: en_reg_pwm_7_0  <= shift_reg[7:0];
+                7'h03: en_reg_pwm_15_8 <= shift_reg[7:0];
+                7'h04: pwm_duty_cycle  <= shift_reg[7:0];
+                default: ;
+            endcase
+
             
         end
     end
